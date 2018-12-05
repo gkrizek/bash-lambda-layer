fdd77695af8ca7f7ac57f417acb9be1feb93b545
Graham Krizek <gkrizek@nodesource.com>
2018-12-05 11:47:26 -0600

    Playing around with exit codes

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 function handler () {
     EVENT_DATA=$1
-
+    unset command_not_found_handle
     echo "this is my function" >&2
     git ls
+    echo $?
     echo $EVENT_DATA
 }
